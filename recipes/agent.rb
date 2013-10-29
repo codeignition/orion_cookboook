f43c5b867029a49ab85d748481474ad4bf823e85
sumit <sugupta4@gmail.com>
2013-10-29 13:28:46 +0530

    intial structure

---
+++ recipes/agent.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: orion_cookboook 
+# Recipe:: default
+#
+# Copyright (C) 2013 timusg
+# 
+# All rights reserved - Do Not Redistribute
+#
---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: orion_cookboook 
+# Recipe:: default
+#
+# Copyright (C) 2013 timusg
+# 
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "agent"
