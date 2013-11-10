68a6f138fb3dccbcfde9e4303f4dc61ef3447836
sumit <sugupta4@gmail.com>
2013-11-10 20:03:22 +0530

    adding ohai in metadata

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,6 @@ maintainer_email "sumit@codeignition.co"
 license          "All rights reserved"
 description      "Installs/Configures orion monitoring in linux node"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
+
+depends 'ohai'
